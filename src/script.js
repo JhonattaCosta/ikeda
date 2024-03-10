2696d9c79cbc7ba02038b0ff4217f80249f3212c
Jhonatta Santos <jhonattasantos27@gmail.com>
2024-03-10 13:52:20 -0300

    Adicionado o Prettier como identador do codigo

--- src/script.js
+++ src/script.js
@@ -1,32 +1,37 @@
+//function para abrir modal
 function openModal(elemento) {
-    var modal = document.getElementById("modal");
-    var modalImg = document.getElementById("imagemModal");
-    modal.style.display = "block";
-    modal.style.opacity = "0"; // Começa com opacidade 0
-    modalImg.src = elemento.src;
-    setTimeout(function () {
-        modal.style.opacity = "1"; // Aumenta a opacidade para 1 após um pequeno atraso
-    }, 50);
+  var modal = document.getElementById("modal");
+  var modalImg = document.getElementById("imagemModal");
+  modal.style.display = "block";
+  modal.style.opacity = "0"; // Começa com opacidade 0
+  modalImg.src = elemento.src;
+  setTimeout(function () {
+    modal.style.opacity = "1"; // Aumenta a opacidade para 1 após um pequeno atraso
+  }, 50);
 }
-
+//function para fechar o modal
 function closeModal() {
-    var modal = document.getElementById("modal");
-    modal.style.display = "none";
+  var modal = document.getElementById("modal");
+  modal.style.display = "none";
 }
 
+//Function que faz a galeria aparecer com opacidade
 function checkAppearing() {
-    var img = document.querySelectorAll('.gallery-img');
-    img.forEach(function(img) {
-        if (img.getBoundingClientRect().top < window.innerHeight) {
-            img.classList.add('appearing'); // Não precisa do ponto antes do nome da classe
-        }
-    });
+  var img = document.querySelectorAll(".gallery-img");
+  img.forEach(function (img) {
+    if (img.getBoundingClientRect().top < window.innerHeight) {
+      img.classList.add("appearing"); // Não precisa do ponto antes do nome da classe
+    }
+  });
 }
 
-window.addEventListener('scroll', checkAppearing);
-window.addEventListener('load', checkAppearing);
+window.addEventListener("scroll", checkAppearing);
+window.addEventListener("load", checkAppearing);
 
-document.getElementById("contactForm").addEventListener("submit", function(event) {
+//function que abre a pagina de contato enviado em uma nova guia
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault(); // Impede o envio do formulário padrão
     var form = event.target;
     var url = form.getAttribute("action");
@@ -34,23 +39,28 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     var formParams = new URLSearchParams(formValues);
     var newWindow = window.open(url, "_blank"); // Abre uma nova aba
     fetch(url, {
-        method: "POST",
-        body: formParams,
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        }
-    }).then(response => {
+      method: "POST",
+      body: formParams,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+      .then((response) => {
         if (response.ok) {
-            // Se o envio for bem-sucedido, exiba uma mensagem na nova aba
-            newWindow.document.write("<h1>Formulário enviado com sucesso!</h1>");
+          // Se o envio for bem-sucedido, exiba uma mensagem na nova aba
+          newWindow.document.write("<h1>Formulário enviado com sucesso!</h1>");
         } else {
-            // Se houver algum problema no envio, exiba uma mensagem de erro
-            newWindow.document.write("<h1>Ocorreu um erro ao enviar o formulário.</h1>");
+          // Se houver algum problema no envio, exiba uma mensagem de erro
+          newWindow.document.write(
+            "<h1>Ocorreu um erro ao enviar o formulário.</h1>"
+          );
         }
-    }).catch(error => {
+      })
+      .catch((error) => {
         // Se houver um erro ao enviar a requisição, exiba uma mensagem de erro
-        newWindow.document.write("<h1>Ocorreu um erro ao enviar o formulário.</h1>");
+        newWindow.document.write(
+          "<h1>Ocorreu um erro ao enviar o formulário.</h1>"
+        );
         console.error("Erro ao enviar o formulário:", error);
-    });
-});
-
+      });
+  });
